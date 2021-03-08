f6c9c98c9b476ef6d9be674a826ad1be3e2032cd
ben <28762961+sonoffavor@users.noreply.github.com>
2021-03-07 19:44:57 -0500

    Add Header, Footer, Editor, & Preview components. Began styling the different components.

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Editor from "./Editor.jsx";
+import Previewer from "./Previewer.jsx";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Editor />
+            <Previewer />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Previewer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Previewer() {
+    return (
+        <div>
+            <h1>Previewer</h1>
+        </div>
+    );
+}
+
+export default Previewer;
